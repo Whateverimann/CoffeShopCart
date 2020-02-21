316d5b1af379eab5a96e3767bc9eb5f37b51c36d
Anna <wrobel.anne@outlook.com>
2020-02-21 20:49:25 +0100

    add addProduct function

--- src/components/App.js
+++ src/components/App.js
@@ -4,61 +4,14 @@ import ShoppingCart from "./ShoppingCart";
 import Adress from "./Adress";
 import DeliveryPayment from "./DeliveryPayment";
 import { Steps, Button, message } from "antd";
-import picture1 from "../images/baristaespresso.jpg";
-import picture2 from "../images/baristacaffecrema.jpg";
-import picture3 from "../images/privatkaffeeafricanblue.jpg";
-import picture4 from "../images/espressomilanostyle.jpg";
-import picture5 from "../images/espressosiciliastyle.jpg";
+
 
 const { Step } = Steps;
-
-const data = [
-      {
-        key: 0,
-        name: "Barista Espresso 0,5kg",
-        amount: 1,
-        price: 38.99,
-        totalPrice: 0,
-        picture: picture1
-      },
-      {
-        key: 1,
-        name: "Barista Caffe Crema 0,5kg",
-        amount: 1,
-        price: 40.99,
-        totalPrice: 0,
-        picture: picture2
-      },
-      {
-        key: 2,
-        name: "Privat Kaffe African Blue 0,5kg",
-        amount: 1,
-        price: 44.99,
-        totalPrice: 0,
-        picture: picture3
-      },
-      {
-        key: 3,
-        name: "Espresso Milano Style 0,5kg",
-        amount: 1,
-        price: 39.99,
-        totalPrice: 0,
-        picture: picture4
-      },
-      {
-        key: 4,
-        name: "Espresso Sicilia Style 0,5kg",
-        amount: 1,
-        price: 41.99,
-        totalPrice: 0,
-        picture: picture5
-      }
-    ]
 
 const steps = [
   {
     title: "Shopping cart",
-    content: <ShoppingCart data={data} />
+    content: <ShoppingCart />
   },
   {
     title: "Adress",
@@ -96,22 +49,27 @@ class App extends Component {
         </Steps>
         <div className="steps-content">{steps[current].content}</div>
         <div className="steps-action">
-          {current > 0 && (
-            <Button className="previous-button" style={{ marginLeft: 8 }} onClick={() => this.prev()}>
-              Previous
-            </Button>
-          )}
           {current < steps.length - 1 && (
-            <Button className="next-button" type="primary" onClick={() => this.next()}>
+            <Button
+              className="next-button"
+              type="primary"
+              onClick={() => this.next()}
+            >
               Next
             </Button>
           )}
           {current === steps.length - 1 && (
-            <Button className="next-button"
+            <Button
+              className="next-button"
               type="primary"
               onClick={() => message.success("Processing complete!")}
             >
               Done
+            </Button>
+          )}
+          {current > 0 && (
+            <Button className="previous-button" onClick={() => this.prev()}>
+              Previous
             </Button>
           )}
         </div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,26 +1,90 @@
 import React, { Component } from "react";
 import "./ShoppingCart.css";
-import { Row, Col, Table, Button, InputNumber } from "antd";
+import { Row, Col, Table, Button, InputNumber, message } from "antd";
+import picture1 from "../images/baristaespresso.jpg";
+import picture2 from "../images/baristacaffecrema.jpg";
+import picture3 from "../images/privatkaffeeafricanblue.jpg";
+import picture4 from "../images/espressomilanostyle.jpg";
+import picture5 from "../images/espressosiciliastyle.jpg";
 
 const { Column } = Table;
 
 class ShoppingCart extends Component {
   state = {
-    products: []
+    products: [],
+    data: [
+      {
+        key: 0,
+        name: "Barista Espresso 0,5kg",
+        amount: 1,
+        price: 38.99,
+        totalPrice: 0,
+        picture: picture1
+      },
+      {
+        key: 1,
+        name: "Barista Caffe Crema 0,5kg",
+        amount: 1,
+        price: 40.99,
+        totalPrice: 0,
+        picture: picture2
+      },
+      {
+        key: 2,
+        name: "Privat Kaffe African Blue 0,5kg",
+        amount: 1,
+        price: 44.99,
+        totalPrice: 0,
+        picture: picture3
+      },
+      {
+        key: 3,
+        name: "Espresso Milano Style 0,5kg",
+        amount: 1,
+        price: 39.99,
+        totalPrice: 0,
+        picture: picture4
+      },
+      {
+        key: 4,
+        name: "Espresso Sicilia Style 0,5kg",
+        amount: 1,
+        price: 41.99,
+        totalPrice: 0,
+        picture: picture5
+      }
+    ]
+  };
+
+  handleWarning = () => {
+    message.warning("Oops! We don't have more products!");
   };
 
   handleAddProduct = () => {
-    const key = Math.floor(Math.random() * this.props.data.length);
-    const product = this.props.data.find(product => product.key === key);
-    console.log(product);
     const products = [...this.state.products];
+    let data = [...this.state.data];
 
-    // miejsce na warunek?!?!?!??!?!?!??!?!
+    if (products.length < 5) {
+      const key = Math.floor(Math.random() * data.length);
+      const product = data.find(product => product.key === key);
+      
+      const hasMatch = Boolean(
+        products.find(p => {
+          return p.key === key;
+        })
+      );
+      //   console.log(hasMatch);
+      if (!hasMatch) {
+        products.push(product);
+      } else return null;
 
-    products.push(product);
-    this.setState({
-      products
-    });
+    //   data.splice(key, 1);
+
+      this.setState({
+        products,
+        data
+      });
+    } else this.handleWarning();
   };
 
   handleReset = () => {
@@ -53,6 +117,7 @@ class ShoppingCart extends Component {
                       max={10}
                       defaultValue={1}
                       onChange={this.changeAmount}
+                      //onChange={(event) => this.changeAmount(event, this.state.products.key)}
                     />
                   )}
                 />
