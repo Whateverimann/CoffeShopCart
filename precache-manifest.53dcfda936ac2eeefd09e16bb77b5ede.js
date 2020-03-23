9ba4f08cea9c9a4ebf5d697bcbf4b5971fc8f393
Anna <wrobel.anne@outlook.com>
2020-03-23 21:06:55 +0100

    Updates

---
+++ precache-manifest.53dcfda936ac2eeefd09e16bb77b5ede.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb69b0f00d5af9c8b3afda817155a8bf",
+    "url": "/CoffeShopCart/index.html"
+  },
+  {
+    "revision": "8f42dadabbc5a2cf4a47",
+    "url": "/CoffeShopCart/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "a7f011b13dea18384549",
+    "url": "/CoffeShopCart/static/css/main.47a6cdd5.chunk.css"
+  },
+  {
+    "revision": "8f42dadabbc5a2cf4a47",
+    "url": "/CoffeShopCart/static/js/2.76c1748a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/CoffeShopCart/static/js/2.76c1748a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f011b13dea18384549",
+    "url": "/CoffeShopCart/static/js/main.7e27e17a.chunk.js"
+  },
+  {
+    "revision": "9e0ce8a7000503451829",
+    "url": "/CoffeShopCart/static/js/runtime-main.0363ff43.js"
+  },
+  {
+    "revision": "7c8ebf4751aeccda16750b29b59994da",
+    "url": "/CoffeShopCart/static/media/baristacaffecrema.7c8ebf47.jpg"
+  },
+  {
+    "revision": "ed3a6ee1c3395a962b63599b7f5423e5",
+    "url": "/CoffeShopCart/static/media/baristaespresso.ed3a6ee1.jpg"
+  },
+  {
+    "revision": "92de93fe31ce90badc7387fa37f3e060",
+    "url": "/CoffeShopCart/static/media/espressomilanostyle.92de93fe.jpg"
+  },
+  {
+    "revision": "6988f578c465f68409a12d82a4420cf5",
+    "url": "/CoffeShopCart/static/media/espressosiciliastyle.6988f578.jpg"
+  },
+  {
+    "revision": "a89be64d2bec662880295a3574e2c63a",
+    "url": "/CoffeShopCart/static/media/privatkaffeeafricanblue.a89be64d.jpg"
+  }
+]);
